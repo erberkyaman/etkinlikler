8ef5376317523f1f9806bf1765e30b885be73991
erberkyaman <113280397+erberkyaman@users.noreply.github.com>
2022-11-23 23:11:08 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class etkinlik {
+    public static void main(String[] args) {
+        int heat;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Sıcaklık Giriniz: ");
+        heat = input.nextInt();
+
+        if (heat<5){
+            System.out.println("Kayak Yapabilirsiniz");
+        }else if (heat<=25){
+            if (heat<=15){
+                System.out.println("Sinemaya Gidebilirsiniz");
+            }if (heat>=10){
+                System.out.println("Piknik Yapabilirsiniz");
+                }
+
+                
+            } else{
+            System.out.println("Yüzmeye Gidebilirsiniz");
+        }
+        }
+    }
